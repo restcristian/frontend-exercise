933c8398679914ce632ff9879441e2296e9b4337
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-06-10 21:31:53 +0200

    feat: added Basic UI for Recipes and Recipes page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,8 +1,18 @@
 import * as constant from "./constant";
 import Login from "../pages/Login";
+import Recipes from "../pages/Recipes";
 export default [
   {
     path: "/",
+    exact: true,
+    component: Recipes,
+  },
+  {
+    path: constant.RECIPES_PAGE,
+    component: Recipes,
+  },
+  {
+    path: constant.LOGIN_PAGE,
     exact: true,
     component: Login,
   },
